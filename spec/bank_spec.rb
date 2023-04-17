a67a89580f7cd3d4f8df1d75ad74de69f8f3a18b
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-04-17 23:04:47 +0100

    Test added - Deposits 1 monetary unit and the date.

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Bank do
     expect(account1.balance).to eq 1
   end
 
+  it 'Deposits 1 monetary unit and returns the deposit balance and date' do
+    account1 = Bank.new
+    transaction1 = account1.deposit(1, '17/04/2023')
+    expect(transaction1).to eq ({'balance'=> 1, 'date'=> '17/04/2023'})
+  end
 end
