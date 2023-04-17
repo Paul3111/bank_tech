356e3f4a1daa980a1101d5cfac7d83f4196225d3
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-04-17 21:14:52 +0100

    Added test for first and last name blanks.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe Client do
   it 'Creates a client with empty space as first name and returns error.' do
     expect{ Client.new('', 'Lazar') }.to raise_error 'First and last name are mandatory'
   end
+
+  it 'Creates a client with empty space as first and last name and returns error.' do
+    expect{ Client.new('', '') }.to raise_error 'First and last name are mandatory'
+  end
 end
