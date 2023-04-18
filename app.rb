0d65b9c145c993c251ddb3f1e0c069fad227764b
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-04-18 15:41:30 +0100

    Test addeed - Prints the statement header.

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 class Statement
-    def view
+  def initialize
+    @transactions = []
+  end
+
+  def add_transaction()
+
+  end
+
+  def view_statement
     return 'date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00'
-    end
+  end
+
+  def statement_header
+    return "date || credit || debit || balance\n"
+  end
 end
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -1,16 +1,23 @@
 require './app'
 
 RSpec.describe Statement do
-  it 'Deposits 1000 monetary unit and returns the transaction history.' do
+  it 'Deposits 1000 monetary units and returns the transaction history.' do
     statement1 = Statement.new
-    expect(statement1.view).to eq 'date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00'
+    expect(statement1.view_statement).to eq 'date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00'
   end
-    
-  xit 'Deposits 1 monetary unit and returns the transaction history.' do
+  
+  it 'Prints the statement header.' do
+    statement1 = Statement.new
+    expect(statement1.statement_header).to eq "date || credit || debit || balance\n"
+  end
+
+  xit 'Deposits twice and returns the transaction history with the dates.' do
     client1 = Client.new('Paul', 'Lazar')
     account1 = Bank.new
-    account1.deposit(100, '17/04/2023')
+    statement1 = Statement.new
+    statement1.add_transaction(account1.deposit(100, '17/04/2023'))
     expect(account1.balance).to eq 100
     expect(client1.view_client).to eq 'Paul Lazar'
+    expect(statement1.view_statement).to eq ''
   end
 end
